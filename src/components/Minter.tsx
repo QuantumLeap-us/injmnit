504f8554d76e78c34df7438ea6bbf59eed5b356e
QuantumLeap <40482494+QuantumLeap-us@users.noreply.github.com>
2023-12-21 22:36:28 +0800

    lcd

--- src/components/Minter.tsx
+++ src/components/Minter.tsx
@@ -111,7 +111,7 @@ const Minter: React.FC = () => {
       setLogs((pre) => [...pre, `成功导入钱包: ${address.address}`]);
 
       const queryClient = await getQueryClient(
-        "https://1rpc.io/tVCUa1UsZudzSVD7/inj-lcd"
+        "https://1rpc.io/4azkQSbDPceRvfTTa/inj-lcd"
       );
       const result = await queryClient.cosmos.bank.v1beta1.balance({
         address: address.address,
